a255a73da1134855b0696b469038915957b0762f
OthomDev <107086987+OthomDev@users.noreply.github.com>
2022-07-25 10:07:06 -0400

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -4,6 +4,7 @@ scanner(
   {
     serverUrl : 'http://localhost:9000/',
     token : "squ_38fefbe29351db475280f7a406f83ac850577d45",
+    
     options: {
       'sonar.projectName': 'Project_1',
       'sonar.projectDescription': '',
