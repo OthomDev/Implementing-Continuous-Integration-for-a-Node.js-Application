02014833ba5003ebd0959794c045aca11f5e4c91
OthomDev <107086987+OthomDev@users.noreply.github.com>
2022-07-25 10:05:16 -0400

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -3,7 +3,7 @@ const scanner = require('sonarqube-scanner');
 scanner(
   {
     serverUrl : 'http://localhost:9000/',
-    token : "squ_1cfb716bc4f48244f76d30cfa4d16b405010f86c",
+    token : "squ_38fefbe29351db475280f7a406f83ac850577d45",
     options: {
       'sonar.projectName': 'Project_1',
       'sonar.projectDescription': '',
